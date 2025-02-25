1cf326fc1aac6bdff198dee806c24a04a922a7d8
AhmedSalah2551 <github.com/AhmedSalah2551>
2025-02-25 04:09:12 +0200

    3v

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const mongoose = require('mongoose');
-const path = require('path');
 
 app.use(express.json());
 app.use(cors());
